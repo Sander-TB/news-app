d1c945e9328d8a19a31fcdebf37a8b16c84419ed
Sander Byrkjeland <sunbyr@gmail.com>
2021-01-29 16:33:38 +0100

    yay

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { URL } from "./config";
+import Article from "./components/Article";
+import './App.css';
+
+function App() {
+
+  const [news, setNews] = useState<InewsItemProps[]>([]); //Our default state is an empty object
+
+  useEffect(() => {
+    fetch(`${URL}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setNews(response.articles);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
+
+  if (news) {
+    return (
+      <div className="App">
+        {
+          news.map((newsItem: InewsItemProps, index: number) => {
+            return (
+              <Article
+                key={index}
+                {...newsItem}
+              />
+            )
+
+          })}
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">No news for you sorry.</div>
+  )
+}
+
+export default App;
